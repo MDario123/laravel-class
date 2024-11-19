34e00226dc51e72ba5d7d7875ed9c2b7e6504c6d
MDario123 <manuel.dario.oliver@gmail.com>
2024-11-19 09:06:57 +0100

    feat: user related shenanigans users don't require email and their unique identifier is the username

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
-    public function login () {
+    public function showLogin () {
         return view('login');
+    }
+
+    public function login (Request $request) {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended();
+        }
+
+        $user_exists = User::find(['username' => $credentials['username']]);
+
+        if ($user_exists->empty()) {
+            $user = new User;
+            $user->username=$credentials['username'];
+            $user->password=$credentials['password'];
+            $user->save();
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                return redirect()->intended();
+            }
+            else {
+                // Supposedly unreachable
+                error_log('What the fuck bro!!! (login edition)');
+                return Response(['status' => 500]);
+            }
+        }
+        else {
+            return 'Incorrect password.';
+        }
+        return 'The end is near.';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $user = Auth::user();
+    if ($user) {
+        return $user->username;
+    }
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'login'])
+Route::get('/login', [AuthController::class, 'showLogin'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
     ->name('login');
